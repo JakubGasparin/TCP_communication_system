0cf365aadb443bc21720c2f93b59149763bb50c7
JakubGasparin <55839847+JakubGasparin@users.noreply.github.com>
2022-11-23 16:18:30 +0100

    Add files via upload

---
+++ c.py
@@ -0,0 +1,52 @@
+import socket
+import libscrc
+
+HOST = "192.168.56.1"
+PORT = 5555
+FRAGMENT_SIZE = 1024
+
+
+def create_packet():
+    packet = ""
+    msg = input("Insert your message: ").encode()
+    order = '12'
+    operation = '001'
+
+    order = order.encode()
+    operation = operation.encode()
+
+    checksum = libscrc.buypass(order + operation + msg)
+    checksum = str(checksum)
+    checksum = checksum.encode()
+
+    msg = order + operation + msg + checksum
+    print(msg)
+    return msg
+
+def decode_acknowledgement_packet(data):
+    msg = data.decode()
+    operation = msg[0:3]
+    checksum = msg[3:]
+    packet = [operation, checksum]
+    return packet
+
+
+def main():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((HOST, PORT))
+        while True:
+            packet = create_packet()
+            print(packet)
+            s.send(packet)
+            #sent = input("> ").encode()
+            #s.send(sent)
+            data = s.recv(FRAGMENT_SIZE)
+            packet_ack = decode_acknowledgement_packet(data)
+
+            print(f"Acknowledged: {packet_ack[0]}\n"
+                  f"Checksum: {packet_ack[1]}")
+            #print("Received:", data.decode())
+
+
+if __name__ == "__main__":
+    main()
---
+++ s.py
@@ -0,0 +1,58 @@
+import socket
+
+import libscrc
+
+HOST = "192.168.56.1"
+PORT = 5555
+FRAGMENT_SIZE = 1024
+
+
+def decode_packet(data):
+    msg = data.decode()
+    order = msg[0:2]
+    operation = msg[2:5]
+    checksum = msg[-5:]
+    msg = msg[:-5]
+    data = [msg[5:]]
+    packet = [order, operation, data, checksum]
+    return packet
+
+
+def encode_acknowledgement_packet():
+    operation = 'ACK'
+    operation = operation.encode()
+    checksum = libscrc.buypass(operation)
+    checksum = str(checksum)
+    checksum = checksum.encode()
+    packet = operation + checksum
+    return packet
+
+
+def main():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((HOST, PORT))
+        s.listen()
+        while True:
+            conn, addr = s.accept()
+            with conn:
+                print("Connected to", addr)
+                while True:
+                    data = conn.recv(FRAGMENT_SIZE)
+                    if not data:
+                        break
+                    print("Received:", data.decode())
+                    decoded_packet = decode_packet(data)
+                    print(f"Order of packet: {decoded_packet[0]}\n"
+                          f"Operation: {decoded_packet[1]}\n"
+                          f"Data: {decoded_packet[2]}\n"
+                          f"Checksum{decoded_packet[3]}\n")
+                    sent_packet = encode_acknowledgement_packet()
+                   # sent = input("> ").encode()
+                    try:
+                        conn.send(sent_packet)
+                    except ConnectionResetError as e:
+                        break
+
+
+if __name__ == "__main__":
+    main()
